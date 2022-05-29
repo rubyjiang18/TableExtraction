f089497a87b4bbf6cf59b43e771fbfa9926cb825
JasonZhangggg <62029938+JasonZhangggg@users.noreply.github.com>
2022-05-29 13:03:40 -0700

    Fixed Cluster Link

--- mongoDB.py
+++ mongoDB.py
@@ -5,7 +5,7 @@ import pandas as pd
 #cluster = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false"
 
 # Link to the MongoDB cluster
-cluster = "mongodb+srv://table_extraction:vlldc3JRnDG2DvW3@cluster0.afvrd.mongodb.net/?retryWrites=true&w=majority"
+cluster = "mongodb://table_extraction:vlldc3JRnDG2DvW3@cluster0-shard-00-00.afvrd.mongodb.net:27017,cluster0-shard-00-01.afvrd.mongodb.net:27017,cluster0-shard-00-02.afvrd.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-12bs5c-shard-0&authSource=admin&retryWrites=true&w=majority"
 
 client = MongoClient(cluster)
 
